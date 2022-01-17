f3cb891c2cccebafe81431ea31813d3d265e95b5
ijeysonU <jeyson.jaramillo2016@uteq.edu.ec>
2022-01-17 00:11:23 -0500

    Avances sin probar, problemas con el emulador desde hace varios días.

--- app/src/main/java/com/example/ejemplo_abar_nview/MainActivity.java
+++ app/src/main/java/com/example/ejemplo_abar_nview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejemplo_abar_nview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(String v1, String v2){
+
     }
 }
